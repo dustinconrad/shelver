5bd69f7bd6e8b3f851a6dcd6354c3d760485258a
Dustin Conrad <dustinaconrad@gmail.com>
2015-04-24 00:21:42 -0400

    add tests for goodreads client

--- src/shelver/goodreads.clj
+++ src/shelver/goodreads.clj
@@ -1,16 +1,25 @@
 (ns shelver.goodreads
   (:require [shelver.oauth :as oauth]
-            [clj-http.client :as clj-http]))
+            [clj-http.client :as clj-http]
+            [clojure.xml :as xml]))
 
 (defprotocol GoodreadsClient
   (auth-user [this]))
+
+(defn- api-helper [oauth-client access-token request-method url params]
+  (let [credentials (oauth/credentials oauth-client access-token request-method url params)
+        request-fn (case request-method
+                     :GET clj-http/get)]
+    (-> (request-fn url {:query-params credentials})
+        (update-in [:body] #(-> (.getBytes %) java.io.ByteArrayInputStream. xml/parse)))))
 
 (defrecord DefaultGoodreadsClient [oauth-client access-token]
   GoodreadsClient
   (auth-user [this]
     (let [url "https://www.goodreads.com/api/auth_user"
-          credentials (oauth/credentials oauth-client access-token :GET url nil)]
-      (clj-http/post url {:query-params credentials}))))
+          request-method :GET
+          params nil]
+      (api-helper oauth-client access-token request-method url params))))
 
 (defn new-goodreads-client [oauth-client access-token]
   (map->DefaultGoodreadsClient {:oauth-client oauth-client :access-token access-token}))
---
+++ test/shelver/goodreads_test.clj
@@ -0,0 +1,23 @@
+(ns shelver.goodreads-test
+  (:require [shelver.oauth :as oauth]
+            [com.stuartsierra.component :as component]
+            [shelver.goodreads :as gr]
+            [clojure.test :refer :all]
+            [environ.core :refer [env]]))
+
+(defn default-oauth-client []
+  (->> (oauth/map->DefaultOAuthClient {:api-key           (env :goodreads-api-key)
+                                       :api-secret        (env :goodreads-api-secret)
+                                       :request-token-url "https://www.goodreads.com/oauth/request_token"
+                                       :access-token-url  "https://www.goodreads.com/oauth/access_token"
+                                       :authorize-url     "https://www.goodreads.com/oauth/authorize"
+                                       :signature-method  :hmac-sha1})
+       component/start))
+
+(deftest test-goodreads-client
+  (testing "Testing"
+    (let [goodreads-client (gr/new-goodreads-client (default-oauth-client) (env :goodreads-access-token))]
+      (testing "auth-user"
+        (let [body (->> (gr/auth-user goodreads-client)
+                        :body)]
+          (is (some #(= [:name ["Conrad"]] ((juxt :tag :content) %)) (xml-seq body))))))))
