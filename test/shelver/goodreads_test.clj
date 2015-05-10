0081513ced497e80c083f9016be72e3b211edb3f
Dustin Conrad <dustinaconrad@gmail.com>
2015-05-10 00:11:09 -0400

    test for get shelf books

--- test/shelver/goodreads_test.clj
+++ test/shelver/goodreads_test.clj
@@ -133,8 +133,81 @@
             (is (= expected-id (-> shelf zip/xml-zip zip/down zip/node :content first)))))))))
 
 (deftest test-get-shelf-books
+  ;<?xml version="1.0" encoding="UTF-8"?>
+  ;<GoodreadsResponse>
+  ;   <Request>
+  ;      <authentication>true</authentication>
+  ;      <key><![CDATA[EV7wnkrFg211dYRJNf8bg]]></key>
+  ;      <method><![CDATA[review_list]]></method>
+  ;   </Request>
+  ;   <reviews start="1" end="1" total="1">
+  ;      <review>
+  ;         <id>1263549541</id>
+  ;         <book>
+  ;            <id type="integer">10281630</id>
+  ;            <isbn nil="true" />
+  ;            <isbn13 nil="true" />
+  ;            <text_reviews_count type="integer">0</text_reviews_count>
+  ;            <title>Clojure in Small Pieces</title>
+  ;            <image_url>https://s.gr-assets.com/assets/nophoto/book/111x148-bcc042a9c91a29c1d680899eff700a03.png</image_url>
+  ;            <small_image_url>https://s.gr-assets.com/assets/nophoto/book/50x75-a91bf249278a81aabab721ef782c4a74.png</small_image_url>
+  ;            <large_image_url />
+  ;            <link>https://www.goodreads.com/book/show/10281630-clojure-in-small-pieces</link>
+  ;            <num_pages />
+  ;            <format />
+  ;            <edition_information />
+  ;            <publisher />
+  ;            <publication_day />
+  ;            <publication_year />
+  ;            <publication_month />
+  ;            <average_rating>5.00</average_rating>
+  ;            <ratings_count>1</ratings_count>
+  ;            <description>A literate programming port of the source for Clojure.</description>
+  ;            <authors>
+  ;               <author>
+  ;                  <id>7145075</id>
+  ;                  <name>Rich Hickey</name>
+  ;                  <role />
+  ;                  <image_url><![CDATA[https://s.gr-assets.com/assets/nophoto/user/u_200x266-e183445fd1a1b5cc7075bb1cf7043306.png]]></image_url>
+  ;                  <small_image_url><![CDATA[https://s.gr-assets.com/assets/nophoto/user/u_50x66-632230dc9882b4352d753eedf9396530.png]]></small_image_url>
+  ;                  <link><![CDATA[https://www.goodreads.com/author/show/7145075.Rich_Hickey]]></link>
+  ;                  <average_rating>5.00</average_rating>
+  ;                  <ratings_count>1</ratings_count>
+  ;                  <text_reviews_count>0</text_reviews_count>
+  ;               </author>
+  ;            </authors>
+  ;            <published />
+  ;         </book>
+  ;         <rating>0</rating>
+  ;         <votes>0</votes>
+  ;         <spoiler_flag>false</spoiler_flag>
+  ;         <spoilers_state>none</spoilers_state>
+  ;         <shelves>
+  ;            <shelf name="to-read" exclusive="true" review_shelf_id="969361495" sortable="true" />
+  ;         </shelves>
+  ;         <recommended_for />
+  ;         <recommended_by />
+  ;         <started_at />
+  ;         <read_at />
+  ;         <date_added>Fri Apr 24 19:24:43 -0700 2015</date_added>
+  ;         <date_updated>Fri Apr 24 19:24:44 -0700 2015</date_updated>
+  ;         <read_count />
+  ;         <body />
+  ;         <comments_count>0</comments_count>
+  ;         <url><![CDATA[https://www.goodreads.com/review/show/1263549541]]></url>
+  ;         <link><![CDATA[https://www.goodreads.com/review/show/1263549541]]></link>
+  ;         <owned>0</owned>
+  ;      </review>
+  ;   </reviews>
+  ;</GoodreadsResponse>
   (testing "testing get shelf books"
     (let [goodreads-client (gr/new-goodreads-client (default-oauth-client) (env :goodreads-access-token) nil)]
       (testing "simple request"
-        (let [resp (gr/get-shelf-books goodreads-client "to-read")]
-          (println resp))))))
+        (let [resp (gr/get-shelf-books goodreads-client "to-read")
+              expected "Clojure in Small Pieces"
+              expected-id "10281630"
+              book (-> resp
+                       :parsed
+                       zip/xml-zip
+                       (zx/xml1-> :reviews :review :book [:title expected]))]
+          (is (= expected-id (-> book zip/down zip/node :content first))))))))
