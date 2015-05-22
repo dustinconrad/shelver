b75b048f30b0e4bdc08a62039289862bb8974a3b
Dustin Conrad <dustinaconrad@gmail.com>
2015-05-22 01:44:19 -0400

    google books sort of going

--- src/shelver/googlebooks.clj
+++ src/shelver/googlebooks.clj
@@ -1,8 +1,17 @@
 (ns shelver.googlebooks
   (:require [shelver.price :refer [PriceClient]]
-            [shelver.util :refer :all]))
+            [clj-http.client :as clj-http]
+            [shelver.util :refer :all]
+            [cheshire.core :as json]))
 
 (defrecord GoogleBooksPriceClient [api-key]
   PriceClient
   (get-price [this book-info]
-    nil))
+    (let [intitle (str "intitle:" "\"" (:title book-info) "\"")
+          inauthor (str "inauthor:" "\"" (first (:authors book-info)) "\"")
+          q (clojure.string/join [intitle inauthor])
+          query-params {:q q :filter "ebooks" :key api-key}
+          url "https://www.googleapis.com/books/v1/volumes"]
+      (-> (clj-http/get url {:query-params query-params})
+          :body
+          (json/parse-string true)))))
---
+++ test/shelver/googlebooks_test.clj
@@ -0,0 +1,16 @@
+(ns shelver.googlebooks-test
+  (:require [clojure.test :refer :all]
+            [environ.core :refer [env]]
+            [shelver.util :refer :all]
+            [clojure.zip :as zip]
+            [clojure.data.zip.xml :as zx]
+            [shelver.googlebooks :as gb]
+            [shelver.price :as price]))
+
+(deftest test-googlebooks-price-client
+  (testing "Testing googlebooks price client"
+    (let [gb-price-client (gb/->GoogleBooksPriceClient (env :google-api-key))]
+      (testing "get price"
+        (let [book (price/->BookInfo nil nil "Judas Unchained" #{"Peter F. Hamilton"})
+              response (price/get-price gb-price-client book)]
+          (println response))))))
