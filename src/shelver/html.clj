414294f9b086e1d8f98050ab52177badcfcc8fb5
Dustin Conrad <dustinaconrad@gmail.com>
2015-06-02 23:39:17 -0400

    sign up url

--- src/shelver/html.clj
+++ src/shelver/html.clj
@@ -19,15 +19,15 @@
                   [:li :a] (html/content caption)
                   [:li :a] (html/set-attr :href url)))
 
-(html/defsnippet nav "templates/nav.html" [:body :div#nav] [current-path]
+(html/defsnippet nav "templates/nav.html" [:body :.navbar] [current-path]
                  [[:ul.nav (html/but :.navbar-right)] [:li html/first-of-type]] (replace-nav-item current-path navigation-items)
                  [:ul.nav.navbar-right [:li html/first-of-type]] (replace-nav-item current-path sign-up-items))
 
 (html/deftemplate base "templates/base.html" [{:keys [uri] :as req} {:keys [title main] :as props}]
                   [:head :title] (html/content title)
-                  [:body :div.navbar] (html/substitute (nav uri))
+                  [:body :#nav] (html/substitute (nav uri))
+                  ;[:body :#main] (or )
                   )
-
 
 (defn index [request]
   (apply str (base request {:title "shelver"})))
